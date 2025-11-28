77793be4c416d58746120cda233e0a318056172b
jaddu17 <shafajaddu@gmail.com>
2025-11-28 10:24:44 +0700

    membuat class AplikasiSiswa

--- app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
@@ -17,5 +17,15 @@ class ContainerDataApp(private val context: Context):
             }
         }
 
+class AplikasiSiswa : Application(){
+    /**
+     * AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan depedensi
+     */
 
+    lateinit var container: ContainerApp
+
+    override fun onCreate(){
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
