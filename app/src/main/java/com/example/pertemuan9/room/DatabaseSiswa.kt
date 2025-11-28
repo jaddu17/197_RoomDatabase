c5669ec33d33daa6ee01bad5fb80807620849767
jaddu17 <shafajaddu@gmail.com>
2025-11-28 10:23:57 +0700

    membuat object dari abstrack class

--- app/src/main/java/com/example/pertemuan9/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/room/DatabaseSiswa.kt
@@ -9,5 +9,17 @@ import androidx.room.RoomDatabase
 abstract class DatabaseSiswa : RoomDatabase(){
     abstract fun siswaDao() : SiswaDao
 
+    companion object{
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
 
+        fun getDatabase(context: Context): DatabaseSiswa{
+            return (Instance?: synchronized(this){
+                Room.databaseBuilder(
+                    context, DatabaseSiswa::class.java,
+                "siswa_database")
+                    .build().also { Instance=it }
+            })
+        }
+    }
 }
